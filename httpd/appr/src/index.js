5e0d3e367536360964be31d634466d8b763fe313
HaiMing.Yin <HaiMing.Yin@gmail.com>
2021-07-08 09:18:53 +0800

    appr: trying add server rending

--- httpd/appr/src/index.js
+++ httpd/appr/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 import './index.css';
 // import App from './App';
 import Feed from './App';
@@ -13,6 +14,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+export function RenderFeedComponent(props) {
+  const body = ReactDOMServer.renderToString(
+    React.createElement(Feed, props)
+  );
+  return body;
+};
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
