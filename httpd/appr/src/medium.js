c15cf111fa3e02ac949d6400b02bd5ac99012529
HaiMing.Yin <HaiMing.Yin@gmail.com>
2021-07-11 18:08:47 +0800

    appr: medium draft not work?

--- httpd/appr/src/medium.js
+++ httpd/appr/src/medium.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { convertToRaw } from 'draft-js';
+import { debounce } from 'throttle-debounce'
 
 import {
     Editor,
     createEditorState,
 } from 'medium-draft';
 import mediumDraftImporter from 'medium-draft/lib/importer';
+import mediumDraftExporter from 'medium-draft/lib/exporter';
 
 
 export default class OnPageEditor extends React.Component {
@@ -16,20 +18,28 @@ export default class OnPageEditor extends React.Component {
             editorState: createEditorState(convertToRaw(mediumDraftImporter(props.content))),
         };
 
-        this.onChange = (editorState) => {
-            this.setState({ editorState });
-        };
+        this.editorRef = React.createRef();
     }
 
     componentDidMount() {
-        this.refs.editor.focus();
+        this.editorRef.current.focus();
     }
+
+    onChange = (editorState) => {
+        this.setState({ editorState });
+        this.updateEntry();
+    }
+
+    updateEntry = debounce(400, () => {
+        var content = mediumDraftExporter(this.state.editorState.getCurrentContent());
+        console.log(content);
+    })
 
     render() {
         const { editorState } = this.state;
         return (
             <Editor
-                ref="editor"
+                ref={this.editorRef}
                 editorState={editorState}
                 onChange={this.onChange} />
         );
