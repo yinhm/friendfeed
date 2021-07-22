b175d07168c79ea5c675d3d67ca6d49abc6b0a6c
HaiMing.Yin <HaiMing.Yin@gmail.com>
2021-07-22 14:21:31 +0800

    web: refactor handle(move)

--- httpd/src/server.go
+++ httpd/src/server.go
@@ -17,8 +17,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/gofrs/uuid"
-	"github.com/golang/glog"
-	"github.com/golang/protobuf/proto"
 	pb "github.com/yinhm/friendfeed/proto"
 	"github.com/yinhm/friendfeed/util"
 	"golang.org/x/net/context"
@@ -170,121 +168,6 @@ func (s *Server) feedReadable(c *gin.Context, feedId string) bool {
 	}
 
 	return false
-}
-
-func (s *Server) FetchFeed(c *gin.Context, req proto.Message) (profile *pb.Profile, feed *pb.Feed, err error) {
-	ctx, cancel := DefaultTimeoutContext()
-	defer cancel()
-
-	var format = false
-
-	switch req.(type) {
-	case *pb.FeedRequest:
-		feed, err = s.client.FetchFeed(ctx, req.(*pb.FeedRequest))
-		format = true
-	case *pb.EntryRequest:
-		feed, err = s.client.FetchEntry(ctx, req.(*pb.EntryRequest))
-	case *pb.SearchRequest:
-		feed, err = s.client.Search(ctx, req.(*pb.SearchRequest))
-		format = true
-	}
-	if err != nil {
-		return
-	}
-	profile, err = s.CurrentUser(c)
-	if err != nil {
-		return
-	}
-	graph, err := s.CurrentGraph(c)
-	if err != nil {
-		return
-	}
-	var basetime time.Time
-	for _, e := range feed.Entries {
-		e.RebuildCommand(profile, graph)
-		basetime, _ = time.Parse(time.RFC3339, e.Date)
-		e.Date = util.FormatTime(basetime)
-
-		if format {
-			e.FormatComments(int32(0))
-			e.FormatLikes(int32(0))
-		}
-		e.RebuildCommentsCommand(profile, graph)
-	}
-	return
-}
-
-func (s *Server) HomeHandler(c *gin.Context) {
-	start := ParseStart(c.Request)
-	req := &pb.FeedRequest{
-		Id:       "public",
-		Start:    int32(start),
-		PageSize: 30,
-	}
-
-	_, feed, err := s.FetchFeed(c, req)
-	if RequestError(c, err) {
-		glog.Errorf("fetch feed error: %s", err)
-		return
-	}
-
-	showShare := s.feedWritable(c, "home")
-	prevStart := req.Start - req.PageSize
-	if prevStart < 0 {
-		prevStart = 0
-	}
-	data := pongo2.Context{
-		"show_share":  showShare,
-		"title":       feed.Id,
-		"name":        feed.Id,
-		"feed":        feed,
-		"prev_start":  prevStart,
-		"next_start":  req.Start + req.PageSize,
-		"show_paging": true,
-	}
-	s.renderFeed(c, data)
-}
-
-func (s *Server) FeedHandler(c *gin.Context) {
-	feedname := c.Params.ByName("name")
-	if feedname == "" {
-		feedname = "Home"
-	}
-	start := ParseStart(c.Request)
-	req := &pb.FeedRequest{
-		Id:       feedname,
-		Start:    int32(start),
-		PageSize: 30,
-	}
-	_, feed, err := s.FetchFeed(c, req)
-	if RequestError(c, err) {
-		return
-	}
-	if feed.Private && !s.feedReadable(c, feed.Id) {
-		c.HTML(http.StatusForbidden, "403.html", pongo2.Context{})
-		return
-	}
-
-	showHeader := feed.Id != "Home" && !strings.HasPrefix(feed.Id, "e/")
-	showShare := feed.Id == "Home" || contains(feed.Commands, "post")
-	showDirect := contains(feed.Commands, "dm")
-	prevStart := req.Start - req.PageSize
-	if prevStart < 0 {
-		prevStart = 0
-	}
-	data := pongo2.Context{
-		"show_header": showHeader,
-		"show_share":  showShare,
-		"show_direct": showDirect,
-		"title":       feed.Id,
-		"name":        feed.Id,
-		"feed":        feed,
-		"ff_username": "me",
-		"prev_start":  prevStart,
-		"next_start":  req.Start + req.PageSize,
-		"show_paging": true,
-	}
-	s.renderFeed(c, data)
 }
 
 func contains(slice []string, item string) bool {
@@ -467,37 +350,6 @@ func (s *Server) CommentDeleteHandler(c *gin.Context) {
 	entry.FormatComments(int32(0))
 	entry.RebuildCommentsCommand(profile, graph)
 	c.JSON(200, entry.Comments)
-}
-
-func (s *Server) PublicHandler(c *gin.Context) {
-	start := ParseStart(c.Request)
-	req := &pb.FeedRequest{
-		Id:       "public",
-		Start:    int32(start),
-		PageSize: 30,
-	}
-
-	_, feed, err := s.FetchFeed(c, req)
-	if RequestError(c, err) {
-		return
-	}
-
-	showShare := s.feedWritable(c, "public")
-	prevStart := req.Start - req.PageSize
-	if prevStart < 0 {
-		prevStart = 0
-	}
-	data := pongo2.Context{
-		"show_share":  showShare,
-		"title":       feed.Id,
-		"name":        feed.Id,
-		"feed":        feed,
-		"prev_start":  prevStart,
-		"next_start":  req.Start + req.PageSize,
-		"show_paging": true,
-	}
-	// s.HTML(c, 200, "_feed.html", data)
-	s.renderFeed(c, data)
 }
 
 func RequestError(c *gin.Context, err error) bool {
