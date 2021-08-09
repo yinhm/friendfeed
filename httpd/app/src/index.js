527e676220535eb78e33f2a99edd27044f7adcb2
HaiMing.Yin <HaiMing.Yin@gmail.com>
2021-08-09 15:04:59 +0800

    render app when element exists

--- httpd/app/src/index.js
+++ httpd/app/src/index.js
@@ -3,31 +3,27 @@ import ReactDOM from 'react-dom';
 // import ReactDOMServer from 'react-dom/server';
 import './index.css';
 import './App.css';
-import {App} from './App';
-import {Search} from './search';
+import { App } from './App';
+import { Search } from './search';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (document.getElementById('root')) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Search />
-  </React.StrictMode>,
-  document.getElementById('search')
-);
-
-// export function RenderFeedComponent(props) {
-//   const body = ReactDOMServer.renderToString(
-//     React.createElement(Feed, props)
-//   );
-//   return body;
-// };
-
+if (document.getElementById('search')) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Search />
+    </React.StrictMode>,
+    document.getElementById('search')
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
